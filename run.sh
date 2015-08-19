ca2caa2964f401d930d3d56976cc54254f2068d4
Jordan Sissel <jls@semicomplete.com>
2015-08-18 23:01:31 -0700

    Run kibana. Mount ES data dir from local host. Pass env to pager instance (for pagerduty api key)

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ for i in *; do
   docker rm $i
 done
 
+[ ! -d $HOME/data ] && mkdir $HOME/data
 
 docker run -d -p 80:80 --name web demo:web
-docker run -d --name pager demo:pager
-docker run -d --link pager:pager --name elasticsearch demo:elasticsearch
+docker run -d --env-file ./docker.env --name pager demo:pager
+docker run -d -v $HOME/data:/opt/elasticsearch/data --link pager:pager --name elasticsearch demo:elasticsearch
 docker run -d --link elasticsearch:elasticsearch --name poller demo:poller
-
+docker run -d -p 5601:5601 --link elasticsearch:elasticsearch --name kibana demo:kibana
 
 
